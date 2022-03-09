6e25ee59ef4c29c1ca0a8e9ba35155a3583abc2d
Jokerlk <jokerkenne@gmail.com>
2022-03-09 13:08:53 +0100

    Ajout de la liste des produits avec les cards

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,29 +3,59 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  image: string;
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Rolex X20',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    image:
+      'https://content.rolex.com/dam/2021/upright-bba-with-shadow/m126711chnr-0002.png?impolicy=v6-upright&imwidth=420',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image:
+      'https://pic.clubic.com/v1/images/1930500/raw?fit=smartCrop&width=550&height=450&hash=a83b45443345ffcdcd87c8da32525abfa8e80d5d',
   },
   {
     id: 3,
+    name: 'Samsung Galaxy S22 Ultra',
+    price: 299,
+    description: '',
+    image:
+      'https://pic.clubic.com/v1/images/1975353/raw.webp?fit=smartCrop&width=550&height=450&hash=0fa1e3e10ea3a088de4814674d0be22e81bb9bd6',
+  },
+  {
+    id: 4,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+    image:
+      'https://pic.clubic.com/v1/images/1869918/raw?fit=smartCrop&width=550&height=450&hash=49773cf88ea6c3d30cff8ca852ce7b8bc29f7b36',
+  },
+  {
+    id: 5,
+    name: 'Phone iphone',
+    price: 299,
+    description: '',
+    image:
+      'https://pic.clubic.com/v1/images/1921804/raw?fit=smartCrop&width=550&height=450&hash=0f487114d3d2f36d8b6efa8eb2836e0c7870bf07',
+  },
+  {
+    id: 6,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    image:
+      'https://pic.clubic.com/v1/images/1869918/raw?fit=smartCrop&width=550&height=450&hash=49773cf88ea6c3d30cff8ca852ce7b8bc29f7b36',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
